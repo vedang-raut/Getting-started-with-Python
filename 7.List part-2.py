d98e8a28aa4d73af33e21819265616a92574a261
Vedang Raut <113302628+vedang-raut@users.noreply.github.com>
2023-02-13 15:40:26 +0530

    Add files via upload

---
+++ 7.List part-2.py
@@ -0,0 +1,13 @@
+li=[]
+print("The list is empty",li)
+li.append(5)
+li.append(78)
+li.append(44)
+li.append("Vedang Raut")
+li.append("Python")
+li.append("APPLE California")
+print("\n The list after append",li)
+for i in range(2,6):
+    li.append(i)
+print(" List after addition of elements:")
+print(li)
