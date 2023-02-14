df1ed0eec9457d6d86257a08a5f35317231c0a83
Vedang Raut <113302628+vedang-raut@users.noreply.github.com>
2023-02-14 17:11:53 +0530

    Add files via upload

---
+++ 9.use of break.py
@@ -0,0 +1,14 @@
+li=[22,32,'Python','California',56,78,90]
+print("The current list is:",li)
+tuple=tuple(li)
+print("NOw it is tuple",tuple)
+li=list(tuple)
+print(li)
+for i in range(2,5):
+    li.append(i)
+    print(li)
+    if i<3:
+        print("The loop is stopped")
+        break
+
+
