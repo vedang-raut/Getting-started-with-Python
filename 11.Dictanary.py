66e2ac7489e937a72aad14451e3b56a8f1353e6b
Vedang Raut <113302628+vedang-raut@users.noreply.github.com>
2023-02-14 17:25:38 +0530

    Add files via upload

---
+++ 11.Dictanary.py
@@ -0,0 +1,18 @@
+a={"apple":"Iphone", "samsung":"Galaxy s23", "Oneplus":"Oneplus 11"}
+print("The given type is", type(a))
+print(a)
+print("apple",a["apple"])
+print("After the updation in dictanary")
+a["apple"]="ios"
+a["samsung"]="One ui"
+a["oneplus"]="Oxygen os"
+print(a)
+print("The dict after adding another values")
+a["samsungprocessor"]="Snapdragon gen2"
+a["Applechipset"]="A16 bionic"
+print(a)
+
+print("samsungprocessor=",a["samsungprocessor"])
+
+
+
